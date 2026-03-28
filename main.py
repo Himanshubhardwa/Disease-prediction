967b60e102cc2e8f6b676e175da67a3135a7f776
Himanshu Bhardwa <gamingspray38@gmail.com>
2026-03-29 00:17:54 +0530

    fetch url set

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # sab allow (abhi ke liye)
+    allow_origins=["*"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
